0bbe0d8c9354fe61b525a1d889005e0a989c0fe8
Kevin Pelzel <kevinpelzel22@gmail.com>
2021-01-26 16:49:01 -0700

    updated dependencies and fixed vis-network stuff

--- src/kraken-interactions/graph.ts
+++ src/kraken-interactions/graph.ts
@@ -1,32 +1,6 @@
+import vis from 'vis-network'
+
 export interface Graph {
-  nodes: Node[]
-  edges: Edge[]
-}
-
-interface Node {
-  id: string
-  label: string
-  borderWidth: number
-  color?: NodeColor
-}
-
-export interface NodeColor extends Highlight {
-  highlight: Highlight
-}
-
-interface Highlight {
-  background: string
-  border: string
-}
-
-interface Edge {
-  id: string
-  to: string
-  from: string
-  color?: EdgeColor
-}
-
-interface EdgeColor {
-  color: string
-  highlight: string
+  nodes: vis.Node[]
+  edges: vis.Edge[]
 }
